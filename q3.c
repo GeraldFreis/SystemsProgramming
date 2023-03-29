2cb9de425909564dccb50963749a5382b5fd23c9
Gerald Freislich <freislichg@gmail.com>
2023-03-30 09:30:11 +1030

    safe

--- q3.c
+++ q3.c
@@ -1,13 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct stat {
+    int inode;
+    int nexthing;
+    const char* randomletters;
+    const char* username;
+    const char* usergroup;
+    const char** dates_changed;
+    int number3;
+    int number4;
+    int number5;
+    const char* filename;
+}; 
+
 int stat(const char *pathname, struct stat *buf){
 
 }
 
 
-
 int main(){
+    system("stat q3.c");
+    
     return 0;
 }
 
