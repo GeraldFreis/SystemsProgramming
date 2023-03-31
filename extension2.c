a11f2bd07f1ce941293bdfbdfa6eb9d0d64e5d24
Gerald Freislich <freislichg@gmail.com>
2023-04-01 09:26:41 +1030

    safe

--- extension2.c
+++ extension2.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // incrementing occurrence when encountered again
 struct word {
@@ -7,22 +8,37 @@ struct word {
     int occurrence;
 }
 
-word* incrementing(word *input, char *requiredword, int numberofwords ){
+word* incrementing(struct word *input, char *requiredword, int numberofwords ){
     for(int i = 0; i < numberofwords; i++){
-        word currentword = input[i];
+        struct word currentword = input[i];
         if(currentword.wordname == requiredword){
             input[i].occurrence = currentword.occurrence + 1;
             return input;
         }
     }
     // if we have iterated till end and not found the word
-    word newword; newword.wordname = requiredword; newword.occurrence = 1;
+    struct word newword; newword.wordname = requiredword; newword.occurrence = 1;
     input[numberofwords] = newword;
     return input;
 }
 
 
-char **wordsinline()
+char **wordsinline(char *line){
+    int n = strlen(line); int low=0;
+    int counter=0;
+    char **wordsthing = (char **)malloc(sizeof(char**)*15);
+
+    for(int i = 0; i < n; i++){
+        if(i == ' '){
+            wordsthing[counter] = (char*)malloc(sizeof(char*)*(i-low));
+            for(int j = low; j < i; j++){
+                wordsthing[counter][j-low] = line[j];
+            }
+            counter++;
+        
+        }
+    }
+}
 
 int main(){
     
---
+++ testingpipes/t_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sequence(char *line){
+
+}
+
+int main(int argc, char **argv){
+    int f_none_works[2];
+    pid_t childprocess;
+    // printf("%s", argv[1]);
+        // setting up pipe
+    int i=1;
+    pipe(f_none_works);
+    while(gets())
+
+    return 0;
+}
