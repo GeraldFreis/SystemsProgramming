e371e4286bca68ef96a35051be93fc4288af3008
Gerald Freislich <freislichg@gmail.com>
2023-04-02 09:16:35 +0930

    safe

--- parser.c
+++ parser.c
@@ -14,6 +14,7 @@ void split_line(char *given_line, char *address[])
     int counter = 0;
     char *current_line = (char *) malloc(sizeof(char * ) * 256);
     int last_val = 0;
+    // printf("%s\n",given_line);
     for(int i = 0; i < len; i++)
     {
         // printf("%s\n", current_line);
@@ -23,24 +24,21 @@ void split_line(char *given_line, char *address[])
             for(int j = 0; j < strlen(current_line); j++){
                 thisadd[j] = current_line[j];
             } 
-            // printf("%s\n", current_line);
             thisadd[strlen(current_line)] = '\0';
             address[counter] = thisadd;
             counter++;   
             last_val = i+1; 
+            // freeing and reallocating to ensure that memory does not overload
             free(current_line); current_line = (char * ) malloc(sizeof(char *) * 256);
+            for(int i = 0; i < 256; i++){current_line[i] = NULL;}
             }
     }
-    // printf("%s\n", current_line);
     char * thisadd = (char * ) malloc ( sizeof( char * ) * strlen( current_line ) + 1 );
     for(int i = 0; i < strlen ( current_line ); i++) {
         thisadd[i] = current_line[i];
     }
     thisadd[strlen(current_line)] = '\0';
     address[counter] = thisadd;
-    // address[counter] = current_line;
-    // printf("%s\n", address[0]);
-    
 
 }
 
--- split_line_test.c
+++ split_line_test.c
@@ -4,15 +4,17 @@
 #include <unistd.h>
 
 extern void split_line(char *line, char *address[]); // splits the given line by spaces into an array
-
+extern void executing(char *address[]); // executes the function by forking 
 int main() {
     // char *address[100];
     char line[256];
     while(gets(line)){
-        char *address[100];
+        printf("%s\n", line);
+        char *address[100] = {NULL}; // populating the address with nulls
         split_line(line, address);
-        // // for(int i = 0; i < 4; i++){printf("%s\n", address[0]);}
-        printf("%s\n", address[2]);
-        // execvp(address[0], address);
+        for(int i = 0; i < 4; i++){printf("%s\n", address[i]);}
+        printf("\n");
+        // printf("%s\n", address[2]);
+        execvp(address[0], address);
     }
 }
