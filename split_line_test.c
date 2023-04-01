eeaee3cca7220009dc9671502b3212776c4159f1
Gerald Freislich <freislichg@gmail.com>
2023-04-01 13:20:51 +1030

    safe

--- split_line_test.c
+++ split_line_test.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 extern void split_line(char *line, char *address[]); // splits the given line by spaces into an array
 
 int main() {
     char *address[100];
-    char line[] = "ls";
+    char line[] = "works";
     split_line(line, address);
     
-    for(int i = 0; i < 4; i++){
-        printf("%s\n", address[i]);
-    }
+    // for(int i = 0; i < 4; i++){
+    //     printf("%s\n", address[i]);
+    // }
+    execvp(address[0], address);
 }
