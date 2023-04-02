e73f9100c6fa592c45e2b008a2e44fa9032bbda1
Gerald Freislich <freislichg@gmail.com>
2023-04-02 09:35:08 +0930

    safe

--- executing.c
+++ executing.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <sys/types.h>
 /*
 execute takes in a char ** array and executes it as a command into STDOUT
 */
-void execute(char *array[]){
-   
+int execute(char *array[]){
+    int file_desmond[2];
+    pid_t pipster;
+    int childprocessid = fork();
+
+    // checking that the fork didnt mess uop
+    if(childprocessid < 0){return 505;} // we're going back to 505
+
+    // if all is good
+    if(childprocessid == 0){
+        // duplicating the input side of the pip into stdout
+        close(file_desmond[0]); close(file_desmond[1]);   
+    } else {
+        close(file_desmond[0]); close(file_desmond[1]);
+    }
+    
+    return 0;
 }
--- parser.c
+++ parser.c
@@ -14,31 +14,33 @@ void split_line(char *given_line, char *address[])
     int counter = 0;
     char *current_line = (char *) malloc(sizeof(char * ) * 256);
     int last_val = 0;
-    // printf("%s\n",given_line);
+
     for(int i = 0; i < len; i++)
     {
-        // printf("%s\n", current_line);
         if(given_line[i] != ' '){ current_line[i-last_val] = given_line[i]; }
         else { 
             char * thisadd = (char * ) malloc ( sizeof(char * ) * (strlen( current_line ) + 1)); // add 1 for eol
+            
             for(int j = 0; j < strlen(current_line); j++){
                 thisadd[j] = current_line[j];
             } 
+
             thisadd[strlen(current_line)] = '\0';
             address[counter] = thisadd;
             counter++;   
             last_val = i+1; 
             // freeing and reallocating to ensure that memory does not overload
             free(current_line); current_line = (char * ) malloc(sizeof(char *) * 256);
-            for(int i = 0; i < 256; i++){current_line[i] = NULL;}
+            for(int i = 0; i < 256; i++){current_line[i] = (char)NULL;}
             }
     }
+
     char * thisadd = (char * ) malloc ( sizeof( char * ) * strlen( current_line ) + 1 );
     for(int i = 0; i < strlen ( current_line ); i++) {
         thisadd[i] = current_line[i];
     }
+
     thisadd[strlen(current_line)] = '\0';
     address[counter] = thisadd;
-
 }
 
