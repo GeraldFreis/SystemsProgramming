195768109cc724d97b6d35266d8091c6a5d65cc2
Gerald Freislich <freislichg@gmail.com>
2023-05-11 09:48:30 +0930

    maybe this is the way that I need to go

--- workshop/question1.c
+++ workshop/question1.c
@@ -1,11 +1,12 @@
 #include    <stdlib.h>
 #include    <stdio.h>
 #include	<unistd.h>
+#include    <string.h>
 // taking in a command and then the commands arguments
 int main(int argc, char **argv) {
     char **commands = malloc(sizeof(char **) * (argc)); // last one is NULL
     // iterating over n-1 and saving those into the commands
-    
+
     int i = 0, j = 0;
     for(i = 1; i < argc; i++){
         commands[i-1] = malloc(sizeof(char *) * strlen(argv[i])); // ensuring it is all inside of the function
