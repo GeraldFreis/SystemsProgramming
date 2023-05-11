6894dbe93c35b886eae93881d6ba82d8c0660122
Gerald Freislich <freislichg@gmail.com>
2023-05-11 09:47:15 +0930

    just adding the files

---
+++ workshop/question1.c
@@ -0,0 +1,21 @@
+#include    <stdlib.h>
+#include    <stdio.h>
+#include	<unistd.h>
+// taking in a command and then the commands arguments
+int main(int argc, char **argv) {
+    char **commands = malloc(sizeof(char **) * (argc)); // last one is NULL
+    // iterating over n-1 and saving those into the commands
+    
+    int i = 0, j = 0;
+    for(i = 1; i < argc; i++){
+        commands[i-1] = malloc(sizeof(char *) * strlen(argv[i])); // ensuring it is all inside of the function
+        for(j = 0; j < strlen(argv[i]); j++){
+            commands[i-1][j] = argv[i][j];
+        }
+    }
+
+    commands[argc] = NULL;
+    // now we can just use execvp
+    execvp(commands[0], commands); 
+
+}
