78ca79c23481fdabcfe0171eb7082dd46883e71e
Gerald Freislich <freislichg@gmail.com>
2023-05-28 10:25:14 +0930

    sfae

---
+++ part1.c
@@ -0,0 +1,7 @@
+// this is the file that will handle the first part (where we ensure that work done lands between start and end)
+
+
+#include 	<stdio.h>
+#include 	<stdlib.h>
+#include  	<unistd.h>
+#include 	"slow_functions.h"
